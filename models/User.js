8bb81d74bac250a9dcf073ed0547aa230449d1e2
김선호 <kimseonho@gimseonhoui-MacBookPro.local>
2020-07-23 16:14:27 +0900

    hi

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 const userschema = mongoose.Schema({
@@ -31,6 +33,24 @@ const userschema = mongoose.Schema({
         type: Number
     }
 })
+
+userschema.pre('save', function( next ) {
+
+    var user = this;
+    if (user.isModified('password')) {
+        //비밀번호 암호화
+        bcrypt.genSalt(saltRounds,function(err, salt){
+            if(err) return next(err)
+
+            bcrypt.hash(user.password ,salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+             })
+         })
+    }
+})
+
 
 const User = mongoose.model('user',userschema)
 
